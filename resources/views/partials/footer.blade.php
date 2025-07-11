eafbf2dbc0c68d383642a3573336bb0f46d97969
AlishaNaraC <alishanara04@gmail.com>
2025-07-11 10:49:05 +0700

    Adjust Header and Footer Components

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,15 @@
+<div class="footer py-1 d-flex flex-column flex-md-row flex-stack bg-dark" id="kt_footer">
+    <!--begin::Copyright-->
+    <div class="text-dark order-2 order-md-1 ms-5">
+        <span class="text-muted fw-bold me-2">2025 ©</span>
+        <a href="https://keenthemes.com" target="_blank" class="text-gray-400 text-hover-primary">LAPI Divusi</a>
+    </div>
+    <!--end::Copyright-->
+    <!--begin::Menu-->
+    <ul class="menu menu-gray-400 menu-hover-primary fw-bold order-1 me-5">
+        <li class="menu-item">
+            <a href="https://keenthemes.com" target="_blank" class="menu-link px-2">About</a>
+        </li>
+    </ul>
+    <!--end::Menu-->
+</div>
