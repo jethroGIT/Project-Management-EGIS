b4e11d00ba57b3f39804af8da2de05b2be8c2879
AlishaNaraC <alishanara04@gmail.com>
2025-07-19 14:54:42 +0700

    update seeder, minor fix

--- database/seeders/WorkPackageSeeder.php
+++ database/seeders/WorkPackageSeeder.php
@@ -16,6 +16,7 @@ class WorkPackageSeeder extends Seeder
     {
         $workpackages = [
             [
+                'category_id' => 1,
                 'wp_number' => '3.1',
                 'name' => 'Human Security Risk Awareness Program Planning',
                 'volume_qty' => 1,
@@ -34,6 +35,7 @@ class WorkPackageSeeder extends Seeder
                 // 'completeness' => 100.00,
             ],
             [
+                'category_id' => 2,
                 'wp_number' => '6.2',
                 'name' => 'Revisit Advisory & Consultancy - Information Security Management System (ISMS)',
                 'volume_qty' => 2,
