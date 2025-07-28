569437070e8a9360bf95e10f89dc74ac32a5d8e1
AlishaNaraC <alishanara04@gmail.com>
2025-07-28 09:10:43 +0700

    page dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,6 +2,19 @@
 
 @section('content')
 <div class="container">
-    <h1 class="my-10">Dashboard</h1>
+    <div class="card">
+        <div class="card-header d-flex">
+            <h1 class="my-10 justify-content-center">Proyek EGIS</h1>
+        </div>
+        <div class="card-body">
+            <p>Diagram Funnel untuk melihat keuangan</p>
+            <p>Summary WP melihat finance performance dan wp performance</p>
+            <p>Role/pekerja yang memiliki jumlah mandays kritikal (sama dengan atau lebih dari mandays plan)</p>
+            <p>Bar chart beban mandays role/pekerja untuk WP</p>
+        </div>
+        <div class="card-footer">
+
+        </div>
+    </div>
 </div>
 @endsection
