7a3ba8fff1445b359edf353da342d32fe81d7218
AlishaNaraC <alishanara04@gmail.com>
2025-08-18 20:41:05 +0700

    minor bug fix

--- app/Http/Controllers/WorkPackageController.php
+++ app/Http/Controllers/WorkPackageController.php
@@ -28,7 +28,7 @@ class WorkPackageController extends Controller
     {
         // Jika ada parameter volume_id di query string
         if ($request->has('volume_id')) {
-            return $this->detail($request->get('volume_id'));
+            return $this->detail($request->get('volume_id'), $request);
         }
         
         // Ambil work package volume pertama atau redirect
