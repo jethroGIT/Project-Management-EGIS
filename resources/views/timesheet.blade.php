981d17632b85a0b8e8f2ecf830df18f9deae8f6b
AlishaNaraC <alishanara04@gmail.com>
2025-08-21 10:58:08 +0700

    update route for timesheet per user

--- resources/views/timesheet.blade.php
+++ resources/views/timesheet.blade.php
@@ -97,7 +97,7 @@
                         Lihat Ringkasan Mandays
                     </button>
                     {{-- id user dummy terlebih dulu, nanti ambil dari session user --}}
-                    <button type="button" class="btn btn-light-primary" aria-expanded="false" aria-controls="kelolaAktivitas" onclick="window.location.href='{{ route('timesheet.detail.user', [$volume->volume_id, 1]) }}'">
+                    <button type="button" class="btn btn-light-primary" aria-expanded="false" aria-controls="kelolaAktivitas" onclick="window.location.href='{{ route('timesheet.detail.user', [$volume->volume_id, auth()->user()->user_id]) }}'">
                         Kelola Aktivitas
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
                             <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"/>
