1ca3688d977c1ff2841d7fca51ed151635554726
KeanuRayhan <keanu.rayhan.tif422@polban.ac.id>
2025-09-04 13:42:21 +0700

    WP Category and Work Package management fix name

--- resources/views/workpackage_management.blade.php
+++ resources/views/workpackage_management.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container-fluid">
-    <h1 class="mt-0 mb-5">Management Work Package</h1>
+    <h1 class="mt-0 mb-5">Management Kategori Work Package</h1>
 
     <div class="card card-flush shadow-sm mb-6">
         <div class="card-body">
--- resources/views/wp_category_management.blade.php
+++ resources/views/wp_category_management.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container-fluid">
-    <h1 class="mt-0 mb-5">Manajemen Kategori Work Package</h1>
+    <h1 class="mt-0 mb-5">Manajemen Work Package</h1>
 
     <!-- Card Category -->
      <div class="card card-flush shadow-sm">
